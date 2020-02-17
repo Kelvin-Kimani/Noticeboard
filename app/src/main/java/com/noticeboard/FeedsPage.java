44bc67ccc94eafb89495b056570a259b369c8fd2
Kelvin Kimani <kelvinkim996@gmail.com>
2020-02-17 13:30:09 +0300

    First commit

---
+++ app/src/main/java/com/noticeboard/FeedsPage.java
@@ -0,0 +1,14 @@
+package com.noticeboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeedsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feeds_page);
+    }
+}
---
+++ app/src/main/java/com/noticeboard/login.java
@@ -0,0 +1,17 @@
+package com.noticeboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+}
